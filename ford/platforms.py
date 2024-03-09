ae68d5ee4b2a63f90155d7d73fcae7f605788505
Cameron Clough <cameronjclough@gmail.com>
2024-03-09 15:34:32 +0000

    all ecu platform codes: update with new VINs

--- ford/platforms.py
+++ ford/platforms.py
@@ -1,19 +1,22 @@
 from functools import cache
 
 from openpilot.selfdrive.car.docs_definitions import split_name
-from openpilot.selfdrive.car.ford.values import CAR, CAR_INFO
+from openpilot.selfdrive.car.ford.values import CAR
 
 
 @cache
 def find_openpilot_platform(car_name: str) -> str | None:
   make, model, year = split_name(car_name)
 
-  for platform, car_infos in CAR_INFO.items():
+  for platform in CAR:
     # TODO: handle Focus Mk3 vs Mk4 (pass market as arg?)
     if platform == CAR.FOCUS_MK4:
       continue
 
-    car_infos = car_infos if isinstance(car_infos, list) else [car_infos]
+    if not platform.config.car_info:
+      continue
+
+    car_infos = platform.config.car_info if isinstance(platform.config.car_info, list) else [platform.config.car_info]
     for car_info in car_infos:
       if car_info.make.upper() != make:
         continue
